d2f2698c5369cd6fb814bad6419b04260738f7ab
AlexRV007 <alexfilipino007@gmail.com>
2025-08-20 18:47:03 +0000

    conversion

---
+++ Javascript/Datatype.js
@@ -0,0 +1,15 @@
+console.log("Rushikesh");
+
+let Name = "Rushikesh"
+
+console.log(typeof Name)
+
+let Name2 = Number(Name)
+
+console.log(Name2)
+
+let Value = 656
+let Value2 = Boolean(Value)
+
+console.log(Boolean(Value))
+console.log(typeof(Value2))
